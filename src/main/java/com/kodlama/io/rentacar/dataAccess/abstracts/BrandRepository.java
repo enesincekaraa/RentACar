50b093893ceca8b8be149f414a98e92376f120d6
Enes Incekara <enesincekara61@gmail.com>
2023-03-15 15:30:58 +0300

    RentACar
    With JavaSpring

---
+++ src/main/java/com/kodlama/io/rentacar/dataAccess/abstracts/BrandRepository.java
@@ -0,0 +1,10 @@
+package com.kodlama.io.rentacar.dataAccess.abstracts;
+
+import com.kodlama.io.rentacar.entities.concretes.Brand;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BrandRepository extends JpaRepository<Brand,Integer> {
+
+
+
+}
